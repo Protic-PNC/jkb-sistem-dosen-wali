f169d7830ba43b4a8e8b93f50ada22f129c7f27d
R230905 <afrizalfajri23@gmail.com>
2024-09-03 17:07:10 +0700

    feat: add controller

---
+++ app/Http/Controllers/StudentResignationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StudentResignation;
+use Illuminate\Http\Request;
+
+class StudentResignationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StudentResignation $studentResignation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StudentResignation $studentResignation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, StudentResignation $studentResignation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StudentResignation $studentResignation)
+    {
+        //
+    }
+}

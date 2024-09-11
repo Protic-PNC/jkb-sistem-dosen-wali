ecd17390b9172987530ed31a8fcfd7cd0428e198
R230905 <afrizalfajri23@gmail.com>
2024-09-11 09:57:53 +0700

    fix: refine lecturer and student relationship
    Explicitly specify foreign and owner key in the lecturer and student relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,11 +49,12 @@ class User extends Authenticatable
 
     public function lecturer()
     {
-        return $this->hasOne(Lecturer::class);
+        return $this->hasOne(Lecturer::class, 'user_id', 'id');
     }
 
     public function student()
     {
-        return $this->hasOne(Student::class);
+        return $this->hasOne(Student::class, 'user_id', 'id');
     }
+
 }

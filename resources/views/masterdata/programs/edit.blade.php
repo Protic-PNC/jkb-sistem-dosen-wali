6eeaa0c031365902bea830818fa39dcc61a016e3
R230905 <afrizalfajri23@gmail.com>
2024-09-11 16:00:18 +0700

    fix: add conditional selected option on kaprodi dropdown

--- resources/views/masterdata/programs/edit.blade.php
+++ resources/views/masterdata/programs/edit.blade.php
@@ -50,7 +50,7 @@
                                         class="select2 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                         <option value="" disabled selected>Pilih Kaprodi</option>
                                         @foreach ($kaprodis as $kaprodi)
-                                            <option value="{{ $kaprodi->lecturer_id }}">{{ $kaprodi->lecturer_name }}</option>
+                                            <option @selected($kaprodi->lecturer_id == $program->head_of_program_id) value="{{ $kaprodi->lecturer_id }}">{{ $kaprodi->lecturer_name }}</option>
                                         @endforeach
                                     </select>
                                     </div>

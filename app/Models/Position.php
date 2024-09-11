a39da166c834d32b73bd88857c19716ab935715a
R230905 <afrizalfajri23@gmail.com>
2024-09-11 09:50:19 +0700

    fix(position model): update primary key
    Change primary key from default 'id' to 'position_id' based on position table's primary key

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Position extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'position_id';
     protected $fillable = [
         'position_name',
     ];

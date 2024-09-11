56b416bd1994b79b98525c929c10b1f471f8f05e
R230905 <afrizalfajri23@gmail.com>
2024-09-11 09:56:26 +0700

    feat: add fillable properties and relationships

--- app/Models/TuitionArrearDetail.php
+++ app/Models/TuitionArrearDetail.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class TuitionArrearDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'tuition_arrear_id',
+        'student_id',
+        'amount'
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class, 'student_id', 'student_id');
+    }
+
+    public function tuition_arrear()
+    {
+        return  $this->belongsTo(TuitionArrear::class, 'tuition_arrear_id', 'tuition_arrear_id');
+    }
 }

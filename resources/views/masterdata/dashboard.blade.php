8e07ccb2d9893db11da54f8b37e7b94d0a5ba30f
R230905 <afrizalfajri23@gmail.com>
2024-09-11 11:01:49 +0700

    feat: add view based on login user's role

--- resources/views/masterdata/dashboard.blade.php
+++ resources/views/masterdata/dashboard.blade.php
@@ -12,7 +12,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-                        {{ __("Anjaay") }}
+                        {{ __("Role Admin") }}
                     </div>
                 </div>
             </div>
@@ -30,7 +30,18 @@
             </div>
         </div>
         @endrole
-
+        
+        @role('mahasiswa')
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        {{ __("Role Mahasiswa") }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endrole
     @endsection
 
 

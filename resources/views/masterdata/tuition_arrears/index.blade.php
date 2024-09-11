5bd09043504ddcf300922d4c7596f6bb4ebe8736
R230905 <afrizalfajri23@gmail.com>
2024-09-11 10:58:03 +0700

    feat: add table view

--- resources/views/masterdata/tuition_arrears/index.blade.php
+++ resources/views/masterdata/tuition_arrears/index.blade.php
@@ -1,3 +1,38 @@
-<div>
-    <!-- Well begun is half done. - Aristotle -->
-</div>
+<x-app-layout>
+    
+    @section('content')
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <th scope="col" class="px-6 py-3 border-b">NIM</th>
+                        <th scope="col" class="px-6 py-3 border-b">Nama</th>
+                        <th scope="col" class="px-6 py-3 border-b">Jumlah Tunggakan</th>
+                        <th scope="col" class="px-6 py-3 border-b">Aksi</th>
+                </thead>
+                <tbody>
+                    @foreach ($tuition as $data)
+
+                        @foreach ($data->tuition_arrear_detail as $detail)
+
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border-b">{{ $detail->student->nim }}</td>
+                                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border-b">{{ $detail->student->student_name }}</td>
+                                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border-b">{{ $detail->amount }}</td>
+                                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Detail</a> <br>
+                                    <a href="#" class="font-medium text-yellow-200 dark:text-blue-500 hover:underline">Edit</a>
+                                </td>
+                            </tr>
+                            
+                        @endforeach
+                        
+                    @endforeach
+                </tbody>
+            </table>
+
+        </div>
+    @endsection
+
+</x-app-layout>

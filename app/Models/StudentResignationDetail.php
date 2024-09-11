4d6ab178f92c5406f2a039de34dbfeeeb76bd4e0
R230905 <afrizalfajri23@gmail.com>
2024-09-11 09:55:52 +0700

    feat: add fillable properties and relationships

--- app/Models/StudentResignationDetail.php
+++ app/Models/StudentResignationDetail.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class StudentResignationDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'student_resignation_id',
+        'student_id',
+        'resignation_type',
+        'decree_number',
+        'reason'
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class, 'student_id', 'student_id');
+    }
+
+    public function student_resignation()
+    {
+        return $this->belongsTo(StudentResignation::class, 'student_resignation_id', 'student_resignation_id');
+    }
 }

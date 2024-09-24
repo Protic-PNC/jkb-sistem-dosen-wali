fe2dd2168e6acde6faf4d4263f6513e2bb3b4251
R230905 <afrizalfajri23@gmail.com>
2024-09-24 16:20:09 +0700

    fix: conditional to create from user menu and mahasiswa menu

--- resources/views/masterdata/students/create.blade.php
+++ resources/views/masterdata/students/create.blade.php
@@ -7,17 +7,27 @@
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         @role('admin')
+
+                        @php
+                        $userId = $user;
+                        $isNewStudent = !$user ? 1 : 0; // Jika user null, maka default ke form tambah mahasiswa
+                        @endphp
+
                         <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Pilih atau Tambah Mahasiswa</h2>
-                            <form action="{{ route('masterdata.students.store', $user->id) }}" method="POST" enctype="multipart/form-data">
+                            <form action="{{ route('masterdata.students.store', $user ?? 'null') }}" method="POST" enctype="multipart/form-data">
                                 @csrf
+                                {{-- @if (!$isNewStudent)
+                                    @method('PUT')
+                                @endif --}}
                                 <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
 
                                     {{-- Input untuk cek apakah tambah mhs baru atau tidak --}}
-                                    <input type="hidden" name="is_new_student" id="is_new_student" value="0">
-                                    <input type="hidden" name="user_id" id="user_id" value="{{ $user->id }}">
+                                    <input type="hidden" name="is_new_student" id="is_new_student" value="{{ $isNewStudent }}">
+                                    <input type="hidden" name="user_id" id="user_id" value="{{ $user->id ?? 'null' }}">
+
 
                                     <!-- Dropdown Mahasiswa dengan Pencarian -->
-                                    <div id="select_student_container" style="display: block">
+                                    <div id="select_student_container" @if($isNewStudent) style="display:none;" @else style="display:block;" @endif>
                                         <label for="select_student" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pilih Mahasiswa</label>
                                         <select id="select_student" name="student_id" class="select2 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                             <option value="" disabled selected>Pilih Mahasiswa</option>
@@ -26,24 +36,25 @@
                                             @endforeach
                                         </select>
                                     </div>
-                                    
 
+                                     
+                                
                                     <!-- Tambah Mahasiswa -->
-                                    <div id="add-student-form" style="display:none;">
+                                    <div id="add-student-form" @if(!$isNewStudent) style="display:none;" @endif>
                                         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tambah Mahasiswa</label>
 
                                         <div class="w-full">
                                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
                                             <input type="text" name="student_name" id="student_name"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                                required="">
+                                                >
                                         </div>
 
                                         <div class="w-full">
                                             <label for="nim" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NIM</label>
                                             <input type="text" name="nim" id="nim"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                                required="">
+                                                >
                                         </div>
                                         <div class="w-full">
                                             <label for="nim" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kelas</label>
@@ -60,14 +71,14 @@
                                             <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alamat</label>
                                             <textarea name="student_address" id="student_address"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                                required=""></textarea>
+                                                ></textarea>
                                         </div>
 
                                         <div class="w-full">
                                             <label for="number_phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nomor Telephone</label>
                                             <input type="text" name="student_phone_number" id="student_phone_number"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                                required="">
+                                                >
                                         </div>
 
                                         <div class="w-full">
@@ -77,10 +88,10 @@
                                         </div>
                                     </div>
 
-                                    <!-- Checkbox untuk menambah mahasiswa baru dengan onchange langsung -->
-                                    <div class="w-full">
+                                    <!-- Checkbox untuk menambah mahasiswa baru -->
+                                    <div @if($isNewStudent) hidden @endif class="w-full">
                                         <label for="add_new_student" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                            <input type="checkbox" id="add_new_student" onchange="toggleAddStudentForm(this)" class="mr-2"> Tambah Mahasiswa Baru
+                                            <input type="checkbox" id="add_new_student" onchange="toggleAddStudentForm(this)" class="mr-2" @if($isNewStudent) checked @endif> Tambah Mahasiswa Baru
                                         </label>
                                     </div>
 
@@ -101,6 +112,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 
+    
     <script>
         function toggleAddStudentForm(checkbox) {
             if (checkbox.checked) {
@@ -113,19 +125,19 @@
                 document.getElementById('is_new_student').value = '0'; //menandakan mhs diambil dari database
             }
         }
-    
-        $(document).ready(function() {
-            // Aktifkan select2 untuk dropdown mahasiswa
-            $('#select_student').select2({
-                placeholder: "Pilih Mahasiswa",
-                allowClear: true
-            });
+
+        // $(document).ready(function() {
+        //     // Aktifkan select2 untuk dropdown mahasiswa
+        //     $('#select_student').select2({
+        //         placeholder: "Pilih Mahasiswa",
+        //         allowClear: true
+        //     });
             
-            // Tambahkan validasi jika diperlukan
-            $('#select_student').on('select2:select', function (e) {
-                $(this).valid();
-            });
-        });
+        //     // Tambahkan validasi jika diperlukan
+        //     $('#select_student').on('select2:select', function (e) {
+        //         $(this).valid();
+        //     });
+        // });
 
     </script>
     

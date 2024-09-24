78ee24cecabe4e924cdbddfd211f409e87ba1fc1
R230905 <afrizalfajri23@gmail.com>
2024-09-24 17:07:56 +0700

    fix: view gpa tables

--- resources/views/masterdata/gpas/index.blade.php
+++ resources/views/masterdata/gpas/index.blade.php
@@ -20,33 +20,19 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($gpa as $data)
-                            @foreach ($data->gpa_cumulative as $cumulative )
-                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border-b">{{ $cumulative->student->nim }}</td>
-                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border-b">{{ $cumulative->student->student_name }}</td>
-                                    @for ($i = 1; $i <= $jumlahSemester; $i++)
-                                        @php
-                                            $gpa_semester_value = '-';
-                                        @endphp
-
-                                        @foreach ($cumulative->gpa_semester as $gpa_semester)
-                                            @if ($gpa_semester->semester == $i)
-                                                @php
-                                                    $gpa_semester_value = $gpa_semester->semester_gpa;
-                                                @endphp
-                                            @endif
-                                        @endforeach
-
-                                        <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border-b text-center">{{ $gpa_semester_value }}</td>
-                                    @endfor
-                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border-b text-center">{{ $cumulative->cumulative_gpa ?? '-' }}</td>
-                                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Detail</a> <br>
-                                        <a href="#" class="font-medium text-yellow-200 dark:text-blue-500 hover:underline">Edit</a>
-                                    </td>
-                                </tr>
-                            @endforeach
+                        @foreach ($students as $student)
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border-b">{{ $student->nim }}</td>
+                                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border-b">{{ $student->student_name }}</td>
+                                @for ($i = 1; $i <= $jumlahSemester; $i++)
+                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border-b">{{ $student->gpa_cumulative->gpa_semester->semester_gpa ?? '-' }}</td>
+                                @endfor
+                                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white border-b">{{ $student->gpa_cumulative->cumulative_gpa ?? '-' }}</td>
+                                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Detail</a> <br>
+                                    <a href="#" class="font-medium text-yellow-200 dark:text-blue-500 hover:underline">Edit</a>
+                                </td>
+                            </tr>                            
                         @endforeach
                     </tbody>
                 </table>

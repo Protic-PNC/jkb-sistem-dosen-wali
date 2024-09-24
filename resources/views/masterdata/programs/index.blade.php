73668a36a494a1fe39ec12b99fac4f246d4869c1
R230905 <afrizalfajri23@gmail.com>
2024-09-24 16:18:03 +0700

    feat: add modal to confirm delete data

--- resources/views/masterdata/programs/index.blade.php
+++ resources/views/masterdata/programs/index.blade.php
@@ -103,22 +103,58 @@
                         </td>
                         <td class="px-6 py-4">
                             <a href="{{ route('masterdata.programs.edit', $data->program_id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            <form action="#
-                            {{-- {{ route('masterdata.programs.destroy', $data->id) }} --}}
-                             "
-                             {{-- method="post"
-                             > --}}
-                             >
-                                {{-- @csrf --}}
-                                {{-- @method('DELETE') --}}
-                                <button type="submit" class="font-medium text-red-600 dark:text-blue-500 hover:underline">Hapus</button>
-                            </form>
+                            <button onclick="confirmDelete('{{ route('masterdata.programs.destroy', $data->program_id) }}')" type="button" class="font-medium text-red-600 dark:text-blue-500 hover:underline">Hapus</button>
                         </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
+
+    <!-- Modal Konfirmasi Delete -->
+    <div id="deleteModal" class="hidden fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+        <div id="alert-additional-content-2" class="p-4 mb-4 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
+            <div class="flex items-center">
+              <svg class="flex-shrink-0 w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+              </svg>
+              <span class="sr-only">Konfirmasi</span>
+              <h3 class="text-lg font-medium">Hapus Prodi?</h3>
+            </div>
+            <div class="mt-2 mb-4 text-sm">
+                Apakah Anda yakin ingin menghapus Prodi ini? Tindakan ini tidak dapat diurungkan.
+            </div>
+            <div class="flex">
+                <form id="deleteForm" method="post" action="{{ route('masterdata.programs.destroy', $data->program_id) }}">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="text-white bg-red-800 hover:bg-red-900 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 me-2 text-center inline-flex items-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                        <svg class="me-2 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 14">
+                            <path d="M10 0C4.612 0 0 5.336 0 7c0 1.742 3.546 7 10 7 6.454 0 10-5.258 10-7 0-1.664-4.612-7-10-7Zm0 10a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z"/>
+                        </svg>
+                        Delete
+                    </button>
+                </form>
+                    <button onclick="hideModal()" class="text-red-800 bg-transparent border border-red-800 hover:bg-red-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-red-600 dark:border-red-600 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800" data-dismiss-target="#alert-additional-content-2" aria-label="Close">
+                        Batal
+                    </button>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function confirmDelete(actionUrl) {
+            // Tampilkan modal
+            document.getElementById('deleteModal').classList.remove('hidden');
+            // Set form action dengan URL delete
+            document.getElementById('deleteForm').setAttribute('action', actionUrl);
+        }
+
+        function hideModal() {
+            // Sembunyikan modal
+            document.getElementById('deleteModal').classList.add('hidden');
+        }
+    </script>
 
     @endsection
 </x-app-layout>

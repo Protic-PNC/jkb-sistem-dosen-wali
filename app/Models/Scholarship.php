20152651a6b7ee6b563194416bbd04f03c2e17ca
R230905 <afrizalfajri23@gmail.com>
2024-09-27 14:43:31 +0700

    feat: add primary key property and relations

--- app/Models/Scholarship.php
+++ app/Models/Scholarship.php
@@ -9,12 +9,18 @@ class Scholarship extends Model
 {
     use HasFactory;
 
+    protected $primaryKey = 'sholarship_id';
     protected $fillable = [
         'class_id'
     ];
 
-    public function student_class()
+    public function student_classes()
     {
         return $this->belongsTo(StudentClass::class, 'class_id', 'class_id');
+    }
+
+    public function scholarship_detail()
+    {
+        return $this->hasMany(ScholarshipDetail::class, 'scholarship_id', 'scholarship_id');
     }
 }
--- app/Models/ScholarshipDetail.php
+++ app/Models/ScholarshipDetail.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class ScholarshipDetail extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'scholarship_detail_id';
+    protected $fillable = [
+        'student_id',
+        'scholarship_type'
+    ];
+
+    public function scholarship()
+    {
+        return $this->belongsTo(Scholarship::class, 'scholarship_id', 'scholarship_id');
+    }
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class, 'student_id', 'student_id');
+    }
 }

6ac2c03bcfd8f7c7707e4afa20ea1dfa252f9c71
R230905 <afrizalfajri23@gmail.com>
2024-09-27 16:38:51 +0700

    refractor: delete notification icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
 
             @include('components.sidebar')
             <div class="flex flex-col flex-1 overflow-hidden">
-                <header class="flex items-center justify-between px-6 py-4 bg-white border-b-4 border-indigo-600">
+                <header class="flex items-center justify-between px-6 py-4 bg-white border-b-4">
                     <div class="flex items-center">
                         <button @click="sidebarOpen = true" class="text-gray-500 focus:outline-none lg:hidden">
                             <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -45,77 +45,10 @@
                     </div>
 
                     <div class="flex">
-                        <div class="flex">
+                        <div class="flex mr-5">
                             <span class="text-lg font-regular text-gray-500 dark:text-gray-400">Selamat datang,
                                 <b>{{ Auth::user()->name }}!</b></span>
                         </div>
-                        <div x-data="{ notificationOpen: false }" class="relative">
-                            <button @click="notificationOpen = ! notificationOpen"
-                                class="flex mx-4 text-gray-600 focus:outline-none">
-                                <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
-                                        stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round">
-                                    </path>
-                                </svg>
-                            </button>
-
-                            <div x-show="notificationOpen" @click="notificationOpen = false"
-                                class="fixed inset-0 z-10 w-full h-full" style="display: none;"></div>
-
-                            <div x-show="notificationOpen"
-                                class="absolute right-0 z-10 mt-2 overflow-hidden bg-white rounded-lg shadow-xl w-80"
-                                style="width: 20rem; display: none;">
-
-                                <a href="#"
-                                    class="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-blue-600">
-                                    <img class="object-cover w-8 h-8 mx-1 rounded-full"
-                                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80"
-                                        alt="avatar">
-                                    <p class="mx-2 text-sm">
-                                        <span class="font-bold" href="#">Sara Salah</span> replied on the <span
-                                            class="font-bold text-blue-400" href="#">Upload Image</span> artical
-                                        . 2m
-                                    </p>
-                                </a>
-
-                                <a href="#"
-                                    class="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-blue-600">
-                                    <img class="object-cover w-8 h-8 mx-1 rounded-full"
-                                        src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=634&amp;q=80"
-                                        alt="avatar">
-                                    <p class="mx-2 text-sm">
-                                        <span class="font-bold" href="#">Slick Net</span> start following you .
-                                        45m
-                                    </p>
-                                </a>
-                                <a href="#"
-                                    class="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-blue-600">
-                                    <img class="object-cover w-8 h-8 mx-1 rounded-full"
-                                        src="https://images.unsplash.com/photo-1450297350677-623de575f31c?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80"
-                                        alt="avatar">
-                                    <p class="mx-2 text-sm">
-                                        <span class="font-bold" href="#">Jane Doe</span> Like Your reply on <span
-                                            class="font-bold text-blue-400" href="#">Test with TDD</span>
-                                        artical . 1h
-                                    </p>
-                                </a>
-                                <a href="#"
-                                    class="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-blue-600">
-                                    <img class="object-cover w-8 h-8 mx-1 rounded-full"
-                                        src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=398&amp;q=80"
-                                        alt="avatar">
-                                    <p class="mx-2 text-sm">
-                                        <span class="font-bold" href="#">Abigail Bennett</span> start following
-                                        you . 3h
-                                    </p>
-                                </a>
-
-                            </div>
-                        </div>
-
                         <div x-data="{ dropdownOpen: false }" class="relative">
                             <button @click="dropdownOpen = ! dropdownOpen"
                                 class="relative block w-8 h-8 overflow-hidden rounded-full shadow focus:outline-none">
@@ -150,13 +83,14 @@
 
                 <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
                     <div class="container px-6 pt-6 mx-auto">
-                        <div class="page-header mb-4 flex justify-between items-center ">
+                        <div
+                            class="page-header mb-4 flex justify-between items-center bg-white overflow-hidden shadow-sm sm:rounded-lg px-6 py-4">
                             <nav class="flex" aria-label="Breadcrumb">
                                 <ol class="inline-flex items-center ">
                                     <li class="inline-flex items-center">
                                         <a href="{{ route('dashboard.index') }}"
                                             class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-                                            <svg class="w-3 h-3 me-2.5" aria-hidden="true"
+                                            <svg class="w-3 h-3 me-2.5 text-gray-500" aria-hidden="true"
                                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                                                 viewBox="0 0 20 20">
                                                 <path
@@ -164,17 +98,15 @@
                                             </svg>
                                         </a>
                                     </li>
-                                    <li aria-current="page">
-                                        <div class="flex items-center">
-                                            <a href="{{ route('dashboard.index') }}"
-                                                class="hover:text-blue-600 hover:text-blue-600 text-sm font-medium text-gray-500 dark:text-gray-400">Master
-                                                Data</a>
-                                        </div>
-                                    </li>
+                                    {{-- <li aria-current="page">
+                                        <svg class="w-4 h-4 text-gray-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 5 7 7-7 7"/>
+                                        </svg>
+                                    </li> --}}
                                     <li>
                                         <div class="flex items-center">
                                             <span
-                                                class=" text-sm font-medium text-gray-700 md:ms-2 dark:text-gray-400 dark:hover:text-white">@yield('descendant_folder')</span>
+                                                class=" text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400 dark:hover:text-white">@yield('descendant_folder')</span>
                                         </div>
                                     </li>
                                     <li>
@@ -195,7 +127,8 @@
     <footer class=" mt-10 bg-gray-200 dark:bg-gray-800 w-full absolute bottom-0 ">
         <div class="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-end">
             <span class="text-sm text-gray-500 text-center dark:text-gray-400">
-                © 2024 <a href="https://github.com/Rayhan-Afrizal-Fajri" target="_blank" rel="noopener noreferrer" class="text-gray-600 hover:underline">Rayhan Afrizal Fajri</a>. All Rights
+                © 2024 <a href="https://github.com/Rayhan-Afrizal-Fajri" target="_blank" rel="noopener noreferrer"
+                    class="text-gray-600 hover:underline">Rayhan Afrizal Fajri</a>. All Rights
                 Reserved.
             </span>
         </div>

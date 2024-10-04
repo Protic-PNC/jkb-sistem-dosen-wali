b9f2f351182f16281f15ceb249e148c4f74de358
R230905 <afrizalfajri23@gmail.com>
2024-10-04 15:07:30 +0700

    feat: set data to charts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,13 +9,15 @@ use App\Models\Student;
 use App\Models\StudentClass;
 use App\Models\Program;
 use App\Models\Report;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         $usersCount = User::count();
         $studentsCount = Student::count();
@@ -24,7 +26,57 @@ class DashboardController extends Controller
         $lecturersCount = Lecturer::count();
         $reportsCount = Report::count();
 
-        return view('masterdata.dashboard', compact('usersCount', 'studentsCount', 'student_classesCount', 'lecturersCount', 'programsCount', 'reportsCount'));
+        $semester = $request->input('semester');
+
+        $user = Auth::user();
+        //dd($user->roles->first()->name);
+        $students = Student::with([
+            'student_classes.program',
+            'gpa_cumulative.gpa',
+            'gpa_cumulative.gpa_semester',
+        ])->whereHas('student_classes', function ($query) use ($user) {
+            $query->where('academic_advisor_id', $user->lecturer->lecturer_id);
+        })->when($semester, function ($query) use ($semester) {
+            return $query->whereHas('gpa_cumulative', function ($q) use ($semester) {
+                $q->where('semester', $semester);
+            });
+        })
+            ->get();
+
+        $semester_gpas = DB::table('gpa_semesters')
+            ->join('gpa_cumulatives', 'gpa_cumulatives.gpa_cumulative_id', '=', 'gpa_semesters.gpa_cumulative_id')
+            ->join('students', 'students.student_id', '=', 'gpa_cumulatives.student_id')
+            ->join('classes', 'students.class_id', '=', 'classes.class_id')
+            ->where('classes.academic_advisor_id', $user->lecturer->lecturer_id)
+            ->groupBy('gpa_semesters.semester')
+            ->select(DB::raw('gpa_semesters.semester, ROUND(AVG(gpa_semesters.semester_gpa), 2) as avg_gpa'))
+            ->get();
+
+        $chart_data = [];
+        foreach ($semester_gpas as $gpa) {
+            $chart_data[] = [
+                'x' => 'Semester ' . $gpa->semester,
+                'y' => $gpa->avg_gpa,
+            ];
+        }
+            
+
+
+        // Prepare chart data
+        $gpa_data = $students->map(function ($student) {
+            return $student->gpa_cumulative->cumulative_gpa;
+        });
+
+        $categories = $students->map(function ($student) {
+            return $student->student_classes->class_name;
+        });
+
+        // Mengambil rata-rata cumulative_gpa dengan 2 angka desimal
+        $avg_gpas = DB::table('gpa_cumulatives')
+            ->select(DB::raw('ROUND(AVG(cumulative_gpa), 2) AS avg_cumulative_gpa'))
+            ->value('avg_cumulative_gpa');
+
+        return view('masterdata.dashboard', compact('chart_data','gpa_data', 'categories', 'students', 'avg_gpas', 'usersCount', 'studentsCount', 'student_classesCount', 'lecturersCount', 'programsCount', 'reportsCount'));
     }
 
     /**

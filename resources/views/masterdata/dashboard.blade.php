7afed0b877b03d4cae62818bd990c67b45336c20
R230905 <afrizalfajri23@gmail.com>
2024-10-22 15:39:34 +0700

    refractor: remove 'Perwalian'

--- resources/views/masterdata/dashboard.blade.php
+++ resources/views/masterdata/dashboard.blade.php
@@ -32,7 +32,7 @@
                                 </div>
                                 <div class="flex flex-col items-center justify-center">
                                     <dt class="mb-2 text-3xl font-extrabold">{{ $reportsCount }}</dt>
-                                    <dd class="text-gray-500 dark:text-gray-400">Laporan Perwalian</dd>
+                                    <dd class="text-gray-500 dark:text-gray-400">Laporan</dd>
                                 </div>
                             </dl>
                         </div>

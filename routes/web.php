ca2decfd5469401208fe012f7a4f221f14194bdc
R230905 <afrizalfajri23@gmail.com>
2024-10-22 11:11:19 +0700

    feat: add update and delete report route

--- routes/web.php
+++ routes/web.php
@@ -123,6 +123,8 @@ Route::middleware('auth')->group(function () {
         Route::get('/reports/create', [ReportController::class, 'create'])->name('reports.create');
         Route::post('/reports/store', [ReportController::class, 'store'])->name('reports.store');
         Route::post('/reports/edit/{id}', [ReportController::class, 'edit'])->name('reports.edit');
+        Route::put('/reports/update/{id}', [ReportController::class, 'update'])->name('reports.update');
+        Route::delete('/reports/destroy/{id}', [ReportController::class, 'destroy'])->name('reports.destroy');
 
         Route::get('/reports/exportPdf/{id}', [ExportController::class, 'exportPdf'])->name('reports.exportPdf');
         Route::get('/reports/exportWord/{id}', [ExportController::class, 'exportWord'])->name('reports.exportWord');

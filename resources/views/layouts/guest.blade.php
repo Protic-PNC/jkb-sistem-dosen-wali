c2792e7bd503d63e3e2b72bdf1c74a25adaefb6a
R230905 <afrizalfajri23@gmail.com>
2024-10-29 21:19:15 +0700

    refractor: update login and guest view

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'siwali') }}</title>
+    <title>{{ config('app.name', 'siwali') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-sans text-gray-900 bg-gray-100 h-screen">
+    <div class="flex h-full">
+        <div class="flex items-center justify-center w-full bg-white lg:w-1/2">
+            <div class="w-full max-w-md p-6">
                 {{ $slot }}
             </div>
         </div>
-    </body>
+        <div class="hidden lg:flex flex-col justify-center w-1/2 bg-yellow-400 text-white p-12">
+            <h2 class="text-4xl font-bold mb-4">SIWALI JKB</h2>
+            <p class="text-lg mb-6">JKB Academic Advising Information System (SIWALI JKB) is a comprehensive academic
+                advising management system designed to streamline the process of managing student performance,
+                counseling, and other academic data for higher education institutions.</p>
+        </div>
+    </div>
+</body>
 </html>

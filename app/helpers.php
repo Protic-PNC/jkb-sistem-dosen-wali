5fb1dcff052a49133832721c8b376977154cd628
R230905 <afrizalfajri23@gmail.com>
2024-11-25 18:28:32 +0700

    fix: change styling for setActive helper

--- app/helpers.php
+++ app/helpers.php
@@ -3,7 +3,7 @@
 if (!function_exists('setActive')) {
     function setActive($route)
     {
-        return request()->routeIs($route) ? 'text-white bg-gray-600 bg-opacity-25' : 'text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100';
+        return request()->routeIs($route) ? 'h-12 shadow-soft-xl rounded-lg bg-white font-semibold text-slate-700 transition-colors' : '';
     }
 }
 
